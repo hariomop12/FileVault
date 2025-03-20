e115c306f2e41d2b9b259f6fd2a56ef7cb69a74d
Hariom <143026952+hariomop12@users.noreply.github.com>
2025-03-20 11:53:40 +0000

    Add Docker configuration with docker-compose and Dockerfile for application deployment

--- FileVault/src/server.js
+++ FileVault/src/server.js
@@ -32,4 +32,5 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+ 
+app.listen(3000, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
