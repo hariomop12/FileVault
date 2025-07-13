a9fd34fc933d0a27ed9712285f629cfffbb316eb
Hariom <hariomvirkhare02@gmail.com>
2025-07-13 16:53:05 +0530

    Restructure project and update .gitignore
    - Moved all files from FileVault/src/ to root directory
    - Updated .gitignore with comprehensive Node.js exclusions
    - Removed duplicate files and cleaned up structure

--- FileVault/src/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    testEnvironment: 'node',
-    setupFiles: ['./jest.setup.js'],
-    testPathIgnorePatterns: ['/node_modules/'],
-    testMatch: ['**/*.test.js'],
-    verbose: true
-  };
